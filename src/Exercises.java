b619de42477c57bcf56582b61a7a7f6a07249719
John Robertson <john.goceljak@gmail.com>
2020-02-05 17:24:37 -0500

    everywhere finished

--- src/Exercises.java
+++ src/Exercises.java
@@ -118,10 +118,30 @@ public class Exercises {
 	}
 	
 	public boolean everywhere(ArrayList<Integer> numbers, int x) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
+		if (numbers == null || numbers.size() < 1) {
+			return false;
+		}	
+		boolean lastPos = false;
+		int gap = 0;
+		for (int i = 0; i < numbers.size(); i++) {
+			if (numbers.get(i) == x) {
+				lastPos = true;
+				gap = 0;
+			} else {
+				if (i == 1 && lastPos == false) {
+					return false;
+				} else if (lastPos == false) {
+					gap++;
+				}
+				lastPos = false;
+			}
+			if (gap == 2) {
+				return false;
+			}
+		}
+			return true;
 	}
+		
 	
 	public boolean consecutive(ArrayList<Integer> numbers) {
 		// write your code here
